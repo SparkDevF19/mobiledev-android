d12ee0ad56bee11f27161146d374f3753ff77269
Daniel Chang <dchan054@fiu.edu>
2019-10-03 10:12:36 -0400

    Created activity and xml file

---
+++ app/src/main/java/com/sparkdev/uber/Destination_Activity.kt
@@ -0,0 +1,12 @@
+package com.sparkdev.uber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Destination_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_)
+    }
+}
