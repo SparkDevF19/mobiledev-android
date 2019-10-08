1c46749068e25688319ade72a854cab33e08da82
Daniel Chang <changdaniel025@gmail.com>
2019-10-08 10:42:03 -0400

    Added search bar and button

--- app/src/main/java/com/sparkdev/uber/Destination_Activity.kt
+++ app/src/main/java/com/sparkdev/uber/Destination_Activity.kt
@@ -8,5 +8,7 @@ class Destination_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_destination_)
+        //TEST
     }
 }
+
