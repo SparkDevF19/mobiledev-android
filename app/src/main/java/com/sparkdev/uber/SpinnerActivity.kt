620cd9439aaef03f12097f8052daa0a3cd330e89
Daniel Chang <changdaniel025@gmail.com>
2019-10-24 20:49:06 -0400

    Created a simple Dropdown Menu

---
+++ app/src/main/java/com/sparkdev/uber/SpinnerActivity.kt
@@ -0,0 +1,44 @@
+package com.sparkdev.uber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+
+class SpinnerActivity : AppCompatActivity() {
+
+    lateinit var option : Spinner
+    lateinit var result : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spinner)
+
+        option = findViewById(R.id.sp_option) as Spinner
+        result = findViewById(R.id.tv_result) as TextView
+
+        val options = arrayOf("Florida International University", "Miami International Airport", "Dolphin Mall")
+
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                result.text = "Please, type your destination"
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                result.text = options.get(position)
+            }
+        }
+
+
+    }
+}
