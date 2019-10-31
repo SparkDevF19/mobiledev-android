40a82c3401de8626efd8901e78ec9a10923bc7b6
acarv613 <acarv037@fiu.edu>
2019-10-31 10:44:02 -0400

    Created empty activity for edit profile page

---
+++ app/src/main/java/com/sparkdev/uber/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.sparkdev.uber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
