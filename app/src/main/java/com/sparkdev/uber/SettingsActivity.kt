e1583c6119de4315427387b2f563a9bc18f84513
acarv613 <acarv037@fiu.edu>
2019-10-31 09:54:55 -0400

    Changes to settings

--- app/src/main/java/com/sparkdev/uber/SettingsActivity.kt
+++ app/src/main/java/com/sparkdev/uber/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.sparkdev.uber
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -14,6 +16,22 @@ class SettingsActivity : AppCompatActivity() {
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val userName: TextView = findViewById(R.id.userName)
+        val userEmail: TextView = findViewById(R.id.userEmail)
+        val userNumber: TextView = findViewById(R.id.userPhoneNumber)
+        val signOutButton: Button = findViewById(R.id.SignOutButton)
+        val editProfileButton: Button = findViewById(R.id.EditProfButton)
+        val homeAddress: EditText = findViewById(R.id.HomeAddress)
+        val workAddress: EditText = findViewById(R.id.WorkAddress)
+        val avatar = findViewById<ImageView>(R.id.ProfileAvatar)
+        signOutButton.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        Toast.makeText(this, "Signed Out!", Toast.LENGTH_LONG)
+        // do sign out stuff
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
