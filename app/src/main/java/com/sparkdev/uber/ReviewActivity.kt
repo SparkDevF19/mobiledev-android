976116e0f240f7796afd0820f41e93ca17825fcc
Michael Perez <mpere692@fiu.edu>
2019-11-05 10:40:14 -0500

    Added review activity

---
+++ app/src/main/java/com/sparkdev/uber/ReviewActivity.kt
@@ -0,0 +1,12 @@
+package com.sparkdev.uber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.review_activity)
+    }
+}
